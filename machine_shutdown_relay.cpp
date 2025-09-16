d594269735dcd3ed8ccf6bc9f347ae27b857d17f
Sara Krsmanovic <sara.krsmanovic.03@gmail.com>
2025-09-16 15:00:31 +0200

    machine shutdown relay

--- machine_shutdown_relay.cpp
+++ machine_shutdown_relay.cpp
@@ -3,6 +3,8 @@
 #include <jsoncpp/json/json.h>
 #include <httplib.h>
 
+//aaaaaaaaaaaaaaaaaaaaaaaaaa
+
 const char *mqtt_host = "localhost";        //broker
 const int mqtt_port = 1883;                 //port na kom broker slusa
 

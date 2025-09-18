50fe248fb81c28728d545325b1cd472d24d27964
rtrk <sara.krsmanovic.03@gmail.com>
2025-09-18 19:25:31 +0200

    merge

--- merged/emergency_call/emergency_call_merged.cpp
+++ merged/emergency_call/emergency_call_merged.cpp
@@ -7,32 +7,35 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <jsoncpp/json/json.h>
-#include "httplib.h"
 #include <string>
 #include <fstream>
-#include <chrono> 
+#include <chrono>
 #include <thread>
-#include <sstream>
+#include <mosquitto.h>
+#include "httplib.h"
 
 const unsigned short multicast_port = 1900;
 const char* multicast_address = "239.255.255.250";
+const char* mqtt_host = "localhost";
+const int mqtt_port = 1883;
+const char* relay_topic = "actuators/emergency_call_module";
+const char* relay_on = "ON";
+const char* relay_off = "OFF";
+const char* http_server_address = "localhost";
+const int http_server_port = 8080;
+const char* http_server_endpoint = "/update_relay_state";
 
-// Promenljiva za proveru da li se desilo CTRL+C 
 volatile sig_atomic_t ctrl_c_received = 0;
 
-// Funkcija za CTRL+C signal
 void ctrl_c_handler(int signal) {
     if (signal == SIGINT) {
         ctrl_c_received = 1;
     }
 }
 
-// Funkcija za generisanje ID-ja uredaja
 std::string generate_unique_id() {
-    // Cetvorocifreni broj
-    // TODO Provera da li postoji na mrezi uredjaj sa istim ID-jem
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
-    int unique_id = std::rand() % 9000 + 1000; 
+    int unique_id = std::rand() % 9000 + 1000;
     return std::to_string(unique_id);
 }
 
@@ -42,51 +45,25 @@ void send_discovery(int sockfd, struct sockaddr_in server_addr) {
                             "MAN: \"ssdp:discover\"\r\n"
                             "MX: 1\r\n"
                             "ST: ssdp:all\r\n"
-                            "Hi from temperature sensor!!!! :)\r\n"
+                            "Emergency call module\r\n"
                             "\r\n";
-                            
-    sendto(sockfd, discovery.c_str(), discovery.length(), 0,(struct sockaddr*)&server_addr, sizeof(server_addr));
+    sendto(sockfd, discovery.c_str(), discovery.length(), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    std::cout << "ECM Sent discovery message: " << discovery << std::endl;
 }
 
-
 void send_json_response(int sockfd, struct sockaddr_in server_addr, std::string id) {
-
-    // Kreiranje JSON objekta za predstavu stanja uredjaja
     Json::Value client_state;
     client_state["id"] = id;
-    client_state["name"] = "Worker temperature sensor";
+    client_state["name"] = "Emergency call module";
     client_state["status"] = "Online";
-    
 
-    // Konvertovanje JSON objekta u string 
     Json::StreamWriterBuilder builder;
     std::string response = Json::writeString(builder, client_state);
 
-    // Slanje podataka o uredjaju
-    sendto(sockfd, response.c_str(), response.length(), 0,(struct sockaddr*)&server_addr, sizeof(server_addr));
+    sendto(sockfd, response.c_str(), response.length(), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    //std::cout << "Sent JSON response: " << response << std::endl;
 }
 
-int receive_confirmation(int sockfd, struct sockaddr_in server_addr) {
-    char buffer[1024];
-    socklen_t server_len = sizeof(server_addr);
-    
-    int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0,(struct sockaddr*)&server_addr, &server_len);
-    if (bytes_received < 0) {
-        std::cerr << "Receive confirmation failed" << std::endl;
-        return 0;
-    }
-
-    buffer[bytes_received] = '\0'; 
-    std::string message(buffer);
-
-    // Ispisivanje potvrde
-    std::cout << "Received confirmation message from controller:" << std::endl;
-    std::cout << message << std::endl;
-
-    return 1;
-}
-
-// Funkcija za prijem potvrde od kontrolera sa timeout-om
 int receive_confirmation_with_timeout(int sockfd, struct sockaddr_in server_addr, int timeout_sec) {
     char buffer[1024];
     socklen_t server_len = sizeof(server_addr);
@@ -94,45 +71,35 @@ int receive_confirmation_with_timeout(int sockfd, struct sockaddr_in server_addr
     tv.tv_sec = timeout_sec;
     tv.tv_usec = 0;
 
-    // Postavljanje vremenskog ogranicenja za socket
     if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tv, sizeof tv) < 0) {
-        std::cerr << "Error setting socket timeout" << std::endl;
+        std::cerr << "Error setting socket timeout ECM" << std::endl;
         return 0;
     }
 
-    // Select funkcija za pracenje dogadjaja na soketu sa vremenskim ogranicenjem (jer je recvfrom blokirajuca funkcija)
     fd_set fds;
     FD_ZERO(&fds);
     FD_SET(sockfd, &fds);
 
-    // Cekanje na dogadjaj na soketu (prijem poruke ili istek timeouta)
     int ready = select(sockfd + 1, &fds, nullptr, nullptr, &tv);
     if (ready < 0) {
-        // Greska u select funkciji
         std::cerr << "Error in select" << std::endl;
         return 0;
     } else if (ready == 0) {
-        // Timeout je istekao, nema podataka za prijem
         std::cout << "Timeout expired, no data received" << std::endl;
         return 0;
     } else {
-        // Podaci su primljeni, pokušaj prijema
         int bytes_received = recvfrom(sockfd, buffer, sizeof(buffer), 0, (struct sockaddr*)&server_addr, &server_len);
         if (bytes_received < 0) {
-            // Prijem neuspeo
             std::cerr << "Receive confirmation failed" << std::endl;
             return 0;
         }
-        // Prijem uspeo
         buffer[bytes_received] = '\0';
         std::string message(buffer);
-        std::cout << "Received confirmation message from controller:" << std::endl;
-        std::cout << message << std::endl;
+        std::cout << "Received confirmation message from controller: " << message << std::endl;
         return 1;
     }
 }
 
-// Funkcija za slanje NOTIFY poruke tipa ssdp:alive
 void send_notify(int sockfd, struct sockaddr_in server_addr, std::string id) {
     std::string notify = "NOTIFY * HTTP/1.1\r\n"
                          "HOST: " + std::string(multicast_address) + ":" + std::to_string(multicast_port) + "\r\n"
@@ -141,42 +108,67 @@ void send_notify(int sockfd, struct sockaddr_in server_addr, std::string id) {
                          "CACHE-CONTROL: max-age=1800\r\n"
                          "LOCATION: http://localhost/device\r\n"
                          "\r\n";
-
-    sendto(sockfd, notify.c_str(), notify.length(), 0,(struct sockaddr*)&server_addr, sizeof(server_addr));
+    sendto(sockfd, notify.c_str(), notify.length(), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    std::cout << "ECM Sent notify message: " << notify << std::endl;
 }
 
-// Funkcija za slanje NOTIFY poruke tipa ssdp:byebye
 void send_byebye(int sockfd, struct sockaddr_in server_addr, std::string id) {
     std::string byebye = "NOTIFY * HTTP/1.1\r\n"
                          "HOST: " + std::string(multicast_address) + ":" + std::to_string(multicast_port) + "\r\n"
                          "NTS: ssdp:byebye\r\n"
                          "USN: uuid:" + id + "\r\n"
                          "\r\n";
+    sendto(sockfd, byebye.c_str(), byebye.length(), 0, (struct sockaddr*)&server_addr, sizeof(server_addr));
+    std::cout << "ECM Sent byebye message: " << byebye << std::endl;
+}
 
-    sendto(sockfd, byebye.c_str(), byebye.length(), 0,(struct sockaddr*)&server_addr, sizeof(server_addr));
+void notify_environment_of_ecm_state(const std::string& state) {
+    httplib::Client cli(http_server_address, http_server_port);
+    httplib::Headers headers = {{"Content-Type", "application/x-www-form-urlencoded"}};
+    httplib::Params params = {{"emergency_call_module", state}};
+    auto res = cli.Post(http_server_endpoint, headers, params);
+    if (res) {
+        if (res->status == 200) {
+            std::cout << "ECM state notified to environment successfully" << std::endl;
+        } else {
+            std::cerr << "Failed to notify ECM state to environment: " << res->status << " - " << res->body << std::endl;
+        }
+    } else {
+        auto err = res.error();
+        std::cerr << "Failed to notify ECM state to environment: " << httplib::to_string(err) << std::endl;
+    }
+}
+
+void on_message_callback(struct mosquitto* mosq, void* userdata, const struct mosquitto_message* message) {
+    if (message->payloadlen) {
+        std::cout << "Message received on topic: " << message->topic << std::endl;
+        std::cout << "Payload: " << (char*)message->payload << std::endl;
+
+        if (strcmp((char*)message->payload, relay_on) == 0) {
+            std::cout << "ECM actuator turned ON" << std::endl;
+            notify_environment_of_ecm_state(relay_on);
+        } else if (strcmp((char*)message->payload, relay_off) == 0) {
+            std::cout << "ECM actuator turned OFF" << std::endl;
+            notify_environment_of_ecm_state(relay_off);
+        }
+    }
 }
 
 int main() {
     int sockfd;
     struct sockaddr_in server_addr;
-    char buffer[1024];
-    
     std::string id = generate_unique_id();
-    //std::string id  = "4878";
 
-    // Pravljenje UDP socket-a
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
         std::cerr << "Socket creation failed" << std::endl;
         return 1;
     }
 
-    // Server address
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr(multicast_address);
     server_addr.sin_port = htons(multicast_port);
 
-    // Join multicast group
     ip_mreq mreq;
     mreq.imr_multiaddr.s_addr = inet_addr(multicast_address);
     mreq.imr_interface.s_addr = htonl(INADDR_ANY);
@@ -184,57 +176,65 @@ int main() {
         std::cerr << "Joining multicast group failed" << std::endl;
         return 1;
     }
-    
-    // Slanje M-SEARCH poruke kontroleru
-   bool controller_found = false; // Da li je kontroler pronadjen
 
-    // Petlja za slanje M-SEARCH poruka dok se kontroler ne pojavi
+    bool controller_found = false;
+
     while (!controller_found) {
-        // Slanje M-SEARCH poruke
         send_discovery(sockfd, server_addr);
-        std::cout << "M-SEARCH sent." << std::endl;
+        std::cout << "M-SEARCH sent from ECM." << std::endl;
 
-        // Čekanje na odgovor
         bool flag_received = receive_confirmation_with_timeout(sockfd, server_addr, 5);
-        //std::cout << flag_received << std:: endl; // Za debagovanje.
 
         if (flag_received) {
-            controller_found = true; // Kontroler je ponadjen, izlazimo iz petlje
-        } 
-
+            controller_found = true;
+        }
     }
-    
-    
-    
-    // Signal handler za Ctrl+C
+
     struct sigaction sig_int_handler;
     sig_int_handler.sa_handler = ctrl_c_handler;
     sigemptyset(&sig_int_handler.sa_mask);
     sig_int_handler.sa_flags = 0;
     sigaction(SIGINT, &sig_int_handler, nullptr);
 
+    mosquitto_lib_init();
+    struct mosquitto* mosq = mosquitto_new("emergency_call_module", true, NULL);
+    if (!mosq) {
+        std::cerr << "Error: Unable to initialize MQTT client.\n";
+        return 1;
+    }
+
+    mosquitto_message_callback_set(mosq, on_message_callback);
+
+    if (mosquitto_connect(mosq, mqtt_host, mqtt_port, 60) != MOSQ_ERR_SUCCESS) {
+        std::cerr << "Error: Unable to connect to MQTT broker.\n";
+        return 1;
+    }
+
+    if (mosquitto_subscribe(mosq, NULL, relay_topic, 0) != MOSQ_ERR_SUCCESS) {
+        std::cerr << "Error: Unable to subscribe to MQTT topic\n";
+        return 1;
+    }
+
     while (!ctrl_c_received) {
-
-        // Slanje NOTIFY ssdp:alive poruke
         send_notify(sockfd, server_addr, id);
-        std::cout << "NOTIFY sent.\n" << std::endl;
-    	
-        // Primanje RESPONSE poruke
-        receive_confirmation(sockfd, server_addr);
+        std::cout << "NOTIFY sent ECM.\n" << std::endl;
 
+        receive_confirmation_with_timeout(sockfd, server_addr, 3);
 
-        // Slanje informacija o uredjaju
         send_json_response(sockfd, server_addr, id);
-        std::cout << "Device status sent.\n" << std::endl;
+        std::cout << "ECM Device status sent.\n" << std::endl;
         std::cout << "************************************" << std::endl;
 
-        // Sleep for 3 seconds
+        mosquitto_loop(mosq, -1, 1);
         std::this_thread::sleep_for(std::chrono::seconds(3));
     }
-    
-    // Slanje NOTIFY ssdp::byebye poruke ako se ugasi uredljaj
+
     send_byebye(sockfd, server_addr, id);
 
+    mosquitto_disconnect(mosq);
+    mosquitto_destroy(mosq);
+    mosquitto_lib_cleanup();
     close(sockfd);
+
     return 0;
 }
